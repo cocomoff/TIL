be3c653597e1404f836ecd29f2dce2d0eb191d92
taki@macmini <taki0313@gmail.com>
2018-02-25 15:11:16 +0900

    percolation at 2dim grid

---
+++ julia/percolation/main.jl
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+using Distributions
+using DataStructures
+using PyPlot
+
+function extend(v)
+    i, j = v
+    return [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+end
+
+
+function generate(p, N)
+    ber = Bernoulli(p)
+    grid = zeros(Int64, N, N)
+    for i in 1:N, j in 1:N
+        grid[i, j] = Int(rand(ber))
+    end
+    if p > 0
+        grid[Int(N/2), Int(N/2)] = 1
+    end
+    return grid
+end
+
+
+function gen_connected(grid)
+    N = size(grid)[1]
+    q = Deque{Tuple{Int, Int}}()
+    con = Set{Tuple{Int, Int}}()
+    v0 = (Int(N/2), Int(N/2))
+    push!(q, v0)
+
+    while length(q) > 0
+        v = pop!(q)
+        push!(con, v)
+        lv = extend(v)
+        for vv in lv
+            ii, jj = vv
+            if 1 <= ii <= N && 1 <= jj <= N
+                if grid[ii, jj] == 1 && !in((ii, jj), con)
+                    push!(q, vv)
+                end
+            end
+        end
+    end
+    return con
+end
+
+
+# generater
+function img_gen(p; N=10)
+    grid = generate(p, N)
+    con = gen_connected(grid)
+    ratio = length(con) / (N * N)
+    conmat = zeros(N, N)
+    for (i, j) in con
+        conmat[i, j] = 1.0
+    end
+    figure(figsize=(6, 3))
+    subplot(1, 2, 1)
+    imshow(grid, cmap="Blues")
+    subplot(1, 2, 2)
+    imshow(conmat, cmap="Blues")
+    savefig("p$(p).png")
+    return ratio
+end
+
+
+rvec = Float64[]
+pp = 0.0:0.005:1.0
+for p in pp
+    grid = generate(p, N)
+    con = gen_connected(grid)
+    ratio = length(con) / (N * N)
+    push!(rvec, ratio)
+end
+
+plot(pp, rvec, "bx--")
+savefig("ratio.png")
